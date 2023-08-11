8747a9942a061b34ee64c17ef61ee94f68c2d3ad
Fahad Alotaibi (فهد العتيبي) <62217808+fahadalkarshmi@users.noreply.github.com>
2023-08-12 00:33:45 +0100

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,22 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -31,13 +13,12 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-});
-
-document.addEventListener("DOMContentLoaded", function() {
+    // Our custom show/hide logic for sections
     const navLinks = document.querySelectorAll(".nav-link.js-scroll-trigger");
     navLinks.forEach(link => {
         link.addEventListener("click", function(e) {
             e.preventDefault();
+            e.stopPropagation(); // Stop event propagation
 
             // Remove the active class from all sections
             const sections = document.querySelectorAll(".fade-in-section");
@@ -50,4 +31,27 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector(targetId).classList.add("active");
         });
     });
+
 });
+Additionally, since we're disabling the scrollspy from Bootstrap, you can safely remove the scrollspy initialization code:
+
+javascript
+Copy code
+// Remove this block from your code
+/*
+const sideNav = document.body.querySelector('#sideNav');
+if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+        target: '#sideNav',
+        rootMargin: '0px 0px -40%',
+    });
+};
+*/
+
+After implementing these changes, the sections should toggle correctly based on the navigation link clicks without interference from other scripts. If you continue to experience issues, please let me know, and we'll troubleshoot further.
+
+
+
+
+
+
