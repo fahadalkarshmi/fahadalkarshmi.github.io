ccd2175912e274d28ef6df15a4f0016b5525c8b5
Fahad Alotaibi (فهد العتيبي) <62217808+fahadalkarshmi@users.noreply.github.com>
2023-08-12 01:52:18 +0100

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,18 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll(".nav-link.js-scroll-trigger");
-    navLinks.forEach(link => {
-        link.addEventListener("click", function(e) {
-            e.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all sections
+    const sections = document.querySelectorAll('.resume-section');
 
+    sections.forEach(function(section) {
+        section.addEventListener('click', function() {
             // Hide all sections
-            const sections = document.querySelectorAll(".fade-in-section");
-            sections.forEach(section => {
-                section.style.display = "none";
-            });
+            sections.forEach(sec => sec.style.display = 'none');
 
-            // Show the section related to the clicked link
-            const targetId = this.getAttribute("href");
-            document.querySelector(targetId).style.display = "block";
+            // Show the clicked section
+            section.style.display = 'block';
         });
     });
 });
